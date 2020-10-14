ef8b056bc9d237aa6bf2d8bb2cbbf8ad7dc96edf
abdulbasit1248 <abdul.basit@thedevden.co>
2020-10-14 12:20:56 +0500

    added cors gem and updated controller

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,2 +1,28 @@
 class V1::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    # c = @contacts.map { |contact| {id: contact.id, first_name: contact.first_name}}
+    # render json: {contacts: c}, status: :ok  
+    render json: @contact, status: :created
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+    render json: @contact, status: :created
+  end
+
+  def destroy
+    @contact = Contact.where(id: params[:id]).first
+    if @contact.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
+  end
 end
