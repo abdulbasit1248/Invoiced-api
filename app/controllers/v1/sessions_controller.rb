df7bf364d65a06b7e63fa7103cf760adfb0b27e6
abdulbasit1248 <abdul.basit@thedevden.co>
2020-10-14 15:15:41 +0500

    returned user info through jbuilder

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,10 +1,11 @@
 class V1::SessionsController < ApplicationController
 
 def create 
-  user = User.where(email: params[:email] ).first
+  @user = User.where(email: params[:email] ).first
   
-  if user.valid_password?(params[:password])
-    render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+  if @user.valid_password?(params[:password])
+    # render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+    render :create, status: :created
   else
     head(:unauthorized)
   end
